238840e838762b92e8499718ba3d0618fc13b895
Ayush <ayushchawda123@gmail.com>
2025-06-30 16:20:31 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Operation_using_Code_First.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
